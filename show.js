0a4139cf910efb5af085deaafb223d9d6584169d
yogarahmatullah <rahmatullahyoga600@gmail.com>
2019-11-28 22:43:49 +0700

    first commit

---
+++ show.js
@@ -0,0 +1,11 @@
+$('#show-password').click(function() {
+	if ($(this).hasClass('fa-eye')) {
+		$('#login-password').attr('type', 'text');
+		$(this).removeClass('fa-eye');
+		$(this).addClass('fa-eye-slash');
+	} else {
+		$('#login-password').attr('type', 'password');
+		$(this).removeClass('fa-eye-slash');
+		$(this).addClass('fa-eye');
+	}
+})
